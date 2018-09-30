3784ea3dcf1f85e6ddbe6f0fa89b4109a5929741
Daniel Tao <danieltaox@gmail.com>
2018-09-29 20:54:26 -0400

    Add trackingPage and navigation between pages, begin frontend design

--- Phoneword/Phoneword/MainPage.xaml.cs
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -5,40 +5,44 @@ namespace Phoneword
 {
     public partial class MainPage : ContentPage
     {
-        string translatedNumber;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        void OnTranslate(object sender, EventArgs e)
+        void OnTracking(object sender, EventArgs e)
         {
-            translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
-            if (!string.IsNullOrWhiteSpace(translatedNumber))
-            {
-                callButton.IsEnabled = true;
-                callButton.Text = "Call " + translatedNumber;
-            }
-            else
-            {
-                callButton.IsEnabled = false;
-                callButton.Text = "Call";
-            }
+            Navigation.PushAsync(new TrackingPage());
         }
 
-        async void OnCall(object sender, EventArgs e)
-        {
-            if (await this.DisplayAlert(
-                    "Dial a Number",
-                    "Would you like to call " + translatedNumber + "?",
-                    "Yes",
-                    "No"))
-            {
-                var dialer = DependencyService.Get<IDialer>();
-                if (dialer != null)
-                    dialer.Dial(translatedNumber);
-            }
-        }
+        //void OnTranslate(object sender, EventArgs e)
+        //{
+        //    translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+        //    if (!string.IsNullOrWhiteSpace(translatedNumber))
+        //    {
+        //        callButton.IsEnabled = true;
+        //        callButton.Text = "Call " + translatedNumber;
+        //    }
+        //    else
+        //    {
+        //        callButton.IsEnabled = false;
+        //        callButton.Text = "Call";
+        //    }
+        //}
+
+        //async void OnCall(object sender, EventArgs e)
+        //{
+        //    if (await this.DisplayAlert(
+        //            "Dial a Number",
+        //            "Would you like to call " + translatedNumber + "?",
+        //            "Yes",
+        //            "No"))
+        //    {
+        //        var dialer = DependencyService.Get<IDialer>();
+        //        if (dialer != null)
+        //            dialer.Dial(translatedNumber);
+        //    }
+        //}
     }
 }
